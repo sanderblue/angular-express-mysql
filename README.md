c8a105ebca21cd0d553d27fd20d71528215b999f
sanderblue <william.a.blue@gmail.com>
2013-09-08 00:26:30 -0500

    install and update dependecies and get things to render again

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-express-angular-mysql
-=====================
-
 An app built with an Angular frontend and an Express + MySQL backend.
-
-Not much to see here just yet...
---
+++ app.js
@@ -0,0 +1,55 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    http    = require('http'),
+    routes  = require('./routes'),
+    api     = require('./routes/api'),
+    app     = express();
+
+// Configuration
+app.configure(function(){
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.set('view options', {
+        layout: false
+    });
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.static(__dirname + '/public'));
+    app.use(app.router);
+});
+
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
+
+// Routes
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+
+// JSON API
+app.get('/api/posts', api.posts);
+app.get('/api/post/:id', api.post);
+app.post('/api/post', api.addPost);
+app.put('/api/post/:id', api.editPost);
+app.delete('/api/post/:id', api.deletePost);
+
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
+
+// Start server
+// app.listen(3000, function(){
+//     console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
+// });
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+});
