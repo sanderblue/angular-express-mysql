e4fb3e425bdcacd9c331b58e2c2389426581c4bf
sanderblue <william.a.blue@gmail.com>
2013-09-12 20:43:52 -0500

    reveal a bit more on Sequelize examples

--- src/sequelize_examples/orm.js
+++ src/sequelize_examples/orm.js
@@ -1,6 +1,6 @@
 var sequelize = new Sequelize('angularexpress', 'root', 'password', {
     // custom host; default: localhost
-    // host: 'my.server.tld',
+    host: 'my.server.tld',
 
     // mysql is the default dialect, but you know...
     // for demo purporses we are defining it nevertheless :)
@@ -8,32 +8,32 @@ var sequelize = new Sequelize('angularexpress', 'root', 'password', {
     dialect: 'mysql'
 
     // custom port; default: 3306
-    // port: 3000,
+    port: 3000,
 
     // custom protocol
     // - default: 'tcp'
     // - added in: v1.5.0
     // - postgres only, useful for heroku
-    // protocol: null,
+    protocol: null,
 
     // disable logging; default: console.log
-    // logging: false,
+    logging: false,
 
     // max concurrent database requests; default: 50
-    // maxConcurrentQueries: 100,
+    maxConcurrentQueries: 100,
 
     // the sql dialect of the database
     // - default is 'mysql'
     // - currently supported: 'mysql', 'sqlite', 'postgres'
-    // dialect: 'mysql',
+    dialect: 'mysql',
 
     // the storage engine for sqlite
     // - default ':memory:'
-    // storage: 'path/to/database.sqlite',
+    storage: 'path/to/database.sqlite',
 
     // disable inserting undefined values as NULL
     // - default: false
-    // omitNull: true,
+    omitNull: true,
 
     // Specify options, which are used when sequelize.define is called.
     // The following example:
@@ -42,31 +42,30 @@ var sequelize = new Sequelize('angularexpress', 'root', 'password', {
     //   sequelize.define(name, attributes, { timestamps: false })
     // so defining the timestamps for each model will be not necessary
     // Below you can see the possible keys for settings. All of them are explained on this page
-    // define: {
-    //     underscored: false
-    //     freezeTableName: false,
-    //     syncOnAssociation: true,
-    //     charset: 'utf8',
-    //     collate: 'utf8_general_ci',
-    //     classMethods: {method1: function() {}},
-    //     instanceMethods: {method2: function() {}},
-    //     timestamps: true
-    // },
+    define: {
+        underscored: false
+        freezeTableName: false,
+        syncOnAssociation: true,
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+        classMethods: {method1: function() {}},
+        instanceMethods: {method2: function() {}},
+        timestamps: true
+    },
 
     // similiar for sync: you can define this to always force sync for models
-    // sync: { force: true },
+    sync: { force: true },
 
     // sync after each association (see below). If set to false, you need to sync manually after setting all associations. Default: true
-    // syncOnAssociation: true,
+    syncOnAssociation: true,
 
     // use pooling in order to reduce db connection overload and to increase speed
     // currently only for mysql and postgresql (since v1.5.0)
-    // pool: { maxConnections: 5, maxIdleTime: 30},
+    pool: { maxConnections: 5, maxIdleTime: 30},
 
     // language is used to determine how to translate words into singular or plural form based on the [lingo project](https://github.com/visionmedia/lingo)
     // options are: en [default], es
-    // language: 'en'
-
+    language: 'en'
 
     // You can also define a custom function for the logging part. Just pass a function.
     // The first parameter will be the string that is logged.
@@ -256,7 +255,6 @@ var User = sequelize.define('User', { firstname: Sequelize.STRING, lastname: Seq
 
 // Example:
 User.build({ firstname: 'foo', lastname: 'bar' }).getFullname(); // 'foo bar'
-
 
 // Find - Search for one specific element in the database
 // search for known ids
